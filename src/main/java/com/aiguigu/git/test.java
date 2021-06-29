d11a89bf8500122a3921a8be643557b89def472d
lyd <lyd@qq.com>
2021-06-29 11:01:56 +0800

    master2  commit

--- src/main/java/com/aiguigu/git/test.java
+++ src/main/java/com/aiguigu/git/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("HELLO3");
         System.out.println(4);
+        System.out.println(master);
     }
 }
